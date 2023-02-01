95947d8e2fda02aff2467a87f017274bdeedbc81
Latishfaction <latishchambhare@gmail.com>
2023-02-01 21:28:28 +0530

    made aadhar card

---
+++ POAM/AadharDB/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+# Create your models here.
+
+class aadhar(models.Model):
+    MALE = 'M'
+    FEMALE = 'F'
+    OTHERS = 'OTH'
+    gender = [
+        (MALE, 'Male'),
+        (FEMALE, 'Female'),
+        (OTHERS, 'Others'),
+    ]
+    first_name=models.CharField(max_length=30)
+    middle_name=models.CharField(max_length=30)
+    last_name=models.CharField(max_length=30)
+    dob = models.DateField()
+    gender = models.CharField(choices=gender,default=OTHERS,max_length=3)
+    aadhar_no = models.BigIntegerField()
+    address = models.TextField()
+    photo = models.ImageField(null=True)
+
+    def __str__(self):
+        return f"Aadhar No : {self.aadhar_no} Name : {self.first_name + self.middle_name + self.last_name} DOB : {self.dob}"
