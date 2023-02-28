0ecdd2d310937dfe6e78639e738b8c57168669fb
Latishfaction <latishchambhare@gmail.com>
2023-02-28 10:43:14 +0530

    deleted unnecessary data

--- POAM/grievenceDB/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class GrievencedbConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'grievenceDB'
--- POAM/grievenceDB/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class grievence(models.Model):
-    property_no = models.IntegerField()
-    #concern department
-    dept = models.CharField()
-    description = models.TextField()
